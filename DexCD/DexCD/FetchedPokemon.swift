cc48f83f471612d2768d2b81638d93c2c92fa82e
Nagaraju <varthalanagaraju@gmail.com>
2025-12-28 03:51:09 +0530

    add toggle to load shiny image

--- DexCD/DexCD/FetchedPokemon.swift
+++ DexCD/DexCD/FetchedPokemon.swift
@@ -61,6 +61,11 @@ struct FetchedPokemon: Decodable {
             let type = try typeContainer.decode(String.self, forKey: .name)
             decodedTypes.append(type)
         }
+        
+        if decodedTypes.count == 2 && decodedTypes[0] == "normal" {
+            decodedTypes.swapAt(0, 1)
+        }
+        
         types = decodedTypes
         
         var decodedStats: [Int16] = []
