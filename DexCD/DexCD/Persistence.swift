bcb090f2ef5cb299b9007c22b984744ea2202ac7
Nagaraju <varthalanagaraju@gmail.com>
2025-12-28 03:15:10 +0530

    details screen

--- DexCD/DexCD/Persistence.swift
+++ DexCD/DexCD/Persistence.swift
@@ -9,8 +9,17 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
+    
+    static var previewPokemon: Pokemon {
+        let context = PersistenceController.preview.container.viewContext
+        
+        let fetchRequest: NSFetchRequest<Pokemon> = Pokemon.fetchRequest()
+        fetchRequest.fetchLimit = 1
+        let results = try! context.fetch(fetchRequest)
+        
+        return results.first!
+    }
 
-    @MainActor
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
